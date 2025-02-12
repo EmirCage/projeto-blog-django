fce1b2e70669178f9ca226acf7c78d2479025903
Emir Eugênio <emireugenio@.com>
2025-02-12 17:12:23 -0300

    salvando models

--- site_setup/models.py
+++ site_setup/models.py
@@ -1,7 +1,7 @@
 # type: ignore
 from django.db import models
 from utils.model_validators import validate_png
-
+from utils.images import resize_image
 
 class MenuLink(models.Model):
     class Meta:
@@ -41,7 +41,15 @@ class SiteSetup(models.Model):
     )
 
     def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
         super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
