6c164ad02c773537b09d40a076bffe6305f4f0d4
Emir Eugênio <emireugenio@.com>
2025-02-14 15:34:38 -0300

    Criando o model Page

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,9 @@ class Tag(models.Model):
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     class Meta:
@@ -34,3 +37,31 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255,
+    )
+
+    is_published = models.BooleanField(
+        default=False,
+        hel_text=(
+            'Este campo preciara estar marcado para a página'
+            'ser exibidda publicamente'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
