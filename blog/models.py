b3af08a03c31e48bf63e103b74cc9b5868d65676
Emir Eugênio <emireugenio@.com>
2025-02-17 14:37:22 -0300

    Filtrando e ordenando os posts enviados para o template da view do Django

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,18 @@ class PostAttachment(AbstractAttachment):
     def save(self, *args, **kwargs):
         if not self.name:
             self.name = self.file.name
-        super().save(*args, **kwargs)
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file, 900)
+
+        return super_save
 
 
 class Tag(models.Model):
